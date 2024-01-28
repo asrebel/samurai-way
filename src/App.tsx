270a825dd91e008745c4b99acbd4cc119ae9bb1d
asrebel <asrebel777@gmail.com>
2024-01-28 19:24:21 +0500

    Sprint 01_01--1-10 lessons done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import {Technologies} from "./Technologies";
+import {Header} from "./Header";
+import {Footer} from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+export const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    );
 }
 
-export default App;
+
+
+
+
+
